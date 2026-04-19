7919b696d6c6d103a8465af31b3a5ec257b2d125
John Mart <juanmart.nt@gmail.com>
2026-04-19 11:44:35 -0600

    feat: add packages in infrastructure and create docker-compose file

---
+++ Mundialito.Infrastructure/Persistence/MundialitoDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mundialito.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mundialito.Infrastructure.Persistence
+{
+    public class MundialitoDbContext : DbContext
+    {
+        public MundialitoDbContext(DbContextOptions<MundialitoDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tournament> Tournaments => Set<Tournament>();
+        public DbSet<Team> Teams => Set<Team>();
+        public DbSet<Player> Players => Set<Player>();
+        public DbSet<Match> Matches => Set<Match>();
+        public DbSet<GoalMatch> GoalMatches => Set<GoalMatch>();
+        public DbSet<TeamTournament> TeamTournaments => Set<TeamTournament>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TeamTournament>().HasKey(teamTournament => new { teamTournament.Id, teamTournament.TeamId });
+
+            modelBuilder.Entity<Match>(entity =>
+            {
+                entity.HasOne(match => match.HomeTeam)
+                .WithMany()
+                .HasForeignKey(match => match.HomeTeamId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(match => match.VisitingTeam)
+                .WithMany()
+                .HasForeignKey(match => match.VisitingTeamId)
+                .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
