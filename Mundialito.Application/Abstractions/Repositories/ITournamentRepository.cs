bdf57902aa95f754854ca5b1b6e64b19ea85771b
John Mart <juanmart.nt@gmail.com>
2026-04-19 15:58:50 -0600

    feat: implement repositoy to handle tournament logic

---
+++ Mundialito.Application/Abstractions/Repositories/ITournamentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Mundialito.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mundialito.Application.Abstractions.Repositories
+{
+    public interface ITournamentRepository
+    {
+        Task AddAsync(Tournament tournament, CancellationToken cancellationToken = default);
+        Task<bool> NameExistsAsync(string name, CancellationToken cancellationToken = default);
+    }
+}
---
+++ Mundialito.Infrastructure/Repositories/TournamentRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mundialito.Application.Abstractions.Repositories;
+using Mundialito.Domain.Entities;
+using Mundialito.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mundialito.Infrastructure.Repositories
+{
+    public class TournamentRepository : ITournamentRepository
+    {
+        private readonly MundialitoDbContext _context;
+
+        public TournamentRepository(MundialitoDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Tournament tournament, CancellationToken cancellationToken)
+        {
+            await _context.AddAsync(tournament, cancellationToken);
+        }
+
+        public async Task<bool> NameExistsAsync(string name, CancellationToken cancellationToken)
+        {
+            return await _context.Tournaments.AnyAsync(tournament =>  tournament.Name == name, cancellationToken);
+        }
+    }
+}
