a73cf54172670d961554cc5c625e443e77885c40
John Mart <juanmart.nt@gmail.com>
2026-04-19 16:47:22 -0600

    feat: add idempotency request entity and create a new migration based on new table

---
+++ Mundialito.Domain/Entities/IdempotencyRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mundialito.Domain.Entities
+{
+    public class IdempotencyRequest
+    {
+        public Guid Id { get; private set; }
+        public string RequestBody { get; private set; } = string.Empty;
+        public string ResponseBody { get; private set; } = string.Empty;
+        public DateTime CreatedAt { get; private set; }
+
+        private IdempotencyRequest() { }
+
+        public IdempotencyRequest(Guid id, string requestBody, string responseBody)
+        {
+            Id = id;
+            RequestBody = requestBody;
+            ResponseBody = responseBody;
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
