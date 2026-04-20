291246332ef054c7b12bed219df15a9ca59e90ac
John Mart <juanmart.nt@gmail.com>
2026-04-19 19:17:23 -0600

    feat: implement connection factory to use dapper in infrastructure and use CQRS to implement queries

---
+++ Mundialito.Application/Abstractions/Data/IConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Mundialito.Application.Abstractions.Data
+{
+    public interface IConnectionFactory
+    {
+        IDbConnection CreateConnection();
+    }
+}
---
+++ Mundialito.Infrastructure/Persistence/SqlConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Mundialito.Application.Abstractions.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Mundialito.Infrastructure.Persistence
+{
+    public class SqlConnectionFactory : IConnectionFactory
+    {
+        private readonly string _connectionString;
+        public SqlConnectionFactory(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
