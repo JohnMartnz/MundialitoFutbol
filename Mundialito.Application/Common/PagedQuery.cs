56590caff9af1ab3c746dca63c4fb365d5143a49
John Mart <juanmart.nt@gmail.com>
2026-04-19 19:17:51 -0600

    feat: implement pagination to get records by page

---
+++ Mundialito.Application/Common/PagedQuery.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mundialito.Application.Common
+{
+    public record PagedQuery(int PageNumber = 1, int PageSize = 10);
+}
---
+++ Mundialito.Application/Common/PagedResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mundialito.Application.Common
+{
+    public record PagedResult<T>(
+        IEnumerable<T> Items,
+        int TotalCount,
+        int PageNumber,
+        int PageSize
+    )
+    {
+        public int TotalPage => (int)Math.Ceiling(TotalCount / (double)PageSize);
+        public bool HasNextPage => PageNumber < TotalPage;
+        public bool HasPreviousPage => PageNumber > 1;
+    }
+}
