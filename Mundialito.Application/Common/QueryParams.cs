1feb8aa36c6b5e93ba8c9a8a46b9ac44e3e7991e
John Mart <juanmart.nt@gmail.com>
2026-04-20 22:40:04 -0600

    feat: create a record for query params

---
+++ Mundialito.Application/Common/QueryParams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mundialito.Application.Common
+{
+    public record QueryParams(
+        int PageNumber = 1,
+        int PageSize = 10,
+        string? Search = null,
+        string? SortBy = null,
+        string? SortDirection = "ASC"
+    );
+}
